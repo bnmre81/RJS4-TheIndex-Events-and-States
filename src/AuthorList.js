e438ca9e66ecc6b52a46d273192a312d9f8d6c1b
Bader Ealyyah <bader.al.ealyyah@gmail.com>
2020-02-25 11:55:47 +0300

    task4

--- src/AuthorList.js
+++ src/AuthorList.js
@@ -1,23 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 
 // Components
 import AuthorCard from "./AuthorCard";
+import SearchBar from "./SearchBar";
 
-const AuthorList = props => {
-  const authorCards = props.authors.map(author => (
-    <AuthorCard
-      key={author.first_name + author.last_name}
-      author={author}
-      selectAuthor={props.selectAuthor}
-    />
-  ));
+class AuthorList extends Component {
+  state = {
+    filteredAuthors: this.props.authors
+  };
+  filterAuthors = query => {
+    const filteredAuthors = this.props.authors.filter(author => {
+      return `${author.first_name} ${author.last_name}`
+        .toLowerCase()
+        .includes(query.toLowerCase());
+    });
+    this.setState({ filteredAuthors: filteredAuthors });
+  };
+  render() {
+    const authorCards = this.state.filteredAuthors.map(author => (
+      <AuthorCard
+        key={author.first_name + author.last_name}
+        author={author}
+        selectAuthor={this.props.selectAuthor}
+      />
+    ));
 
-  return (
-    <div className="authors">
-      <h3>Authors</h3>
-      <div className="row">{authorCards}</div>
-    </div>
-  );
-};
+    return (
+      <div className="authors">
+        <SearchBar filterAuthors={this.filterAuthors} />
+        <h3>Authors</h3>
+        <div className="row">{authorCards}</div>
+      </div>
+    );
+  }
+}
 
 export default AuthorList;
